f8208f21acd3492c14ed8fcab76059939c67dd21
Marco López <mlopezdev@gmail.com>
2017-04-21 02:14:15 -0500

    Add Owners and Refresh

--- src/app/components/owners/owners.component.ts
+++ src/app/components/owners/owners.component.ts
@@ -10,13 +10,25 @@ import { Owner } from "../../owner";
 export class OwnersComponent implements OnInit {
 
   owners: Owner[];
+  owner: Owner = new Owner();
 
   constructor(private petclinicService: PetclinicService) { }
 
   ngOnInit() {
     this.petclinicService.getOwners().subscribe(res => {
       this.owners = res;
-    })
+    });
+  }
+
+  onAddOwner() {
+    this.owner = new Owner();
+  }
+
+  onSaveOwner() {
+    this.petclinicService.saveOwner(this.owner).subscribe(res => {
+      this.owner = res;
+      this.owners.push(res);
+    });
   }
 
 }
